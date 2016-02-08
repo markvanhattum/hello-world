9c5c9aec6e6b49c2d02deebc08d867fa26a68d07
mark <mark@Marcus>
2016-02-08 21:06:35 +0100

    The program has been written.

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/**
+ * This program says 'Hello world!'
+ */
+public class HelloWorld 
+{   /**
+	 * This is where it all starts.
+	 */
+	public static void main(String[] args) 
+	{
+		// Prints 'Hello world!'
+		System.out.println("Hello world!");
+	}
+}
